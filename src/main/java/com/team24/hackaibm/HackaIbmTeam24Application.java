29d637cddddcccf69103ac9d6b1ae43c8090965d
José Guilherme Bestel <josegbestel@gmail.com>
2021-07-24 21:38:46 -0300

    add spring project

---
+++ src/main/java/com/team24/hackaibm/HackaIbmTeam24Application.java
@@ -0,0 +1,13 @@
+package com.team24.hackaibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaIbmTeam24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaIbmTeam24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team24/hackaibm/HackaIbmTeam24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team24.hackaibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackaIbmTeam24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
