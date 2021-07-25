21ff5971f2fc721fbde81a150759c8d8a8174094
José Guilherme Bestel <josegbestel@gmail.com>
2021-07-25 00:55:18 -0300

    add swagger

--- src/main/java/com/team24/hackaibm/HackaIbmTeam24Application.java
+++ src/main/java/com/team24/hackaibm/HackaIbmTeam24Application.java
@@ -2,12 +2,26 @@ package com.team24.hackaibm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class HackaIbmTeam24Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackaIbmTeam24Application.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/ui"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui.html")).build());
 	}
 
 }
